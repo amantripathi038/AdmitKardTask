e577f9c7cd8248556dcdbc8bb4b5d5f3a584e8ee
Aman Tripathi <amancselover@gmail.com>
2023-09-25 10:11:53 +0530

    Rate Limit & File Size Limit

--- controllers/textController.js
+++ controllers/textController.js
@@ -1,5 +1,6 @@
 const formidable = require('formidable');
 const textAnalyzer = require('./../utils/textAnalyzer');
+const SIZE_LIMIT = 5 * 1024 * 1024; // 5 MB
 
 module.exports.analyzeText = async (req, res, next) => {
     const text = req.body.text;
@@ -14,6 +15,7 @@ module.exports.analyzeText = async (req, res, next) => {
 
 module.exports.uploadFile = async (req, res, next) => {
     const form = new formidable.IncomingForm();
+    let flag = false;
     form.parse(req, async (err, fields, files) => {
         if (err) {
             return res.status(500).json({ error: 'Error processing form data.' });
@@ -21,11 +23,17 @@ module.exports.uploadFile = async (req, res, next) => {
         if (!(files.documents)) {
             return res.status(404).json({ error: 'Please upload file.' });
         }
+        files.documents.forEach(file => {
+            if (flag) return;
+            if (file.size > SIZE_LIMIT) {
+                res.status(404).json({ error: 'File size is too big.' });
+                flag = true;
+                return;
+            }
+        });
+        if (flag) return;
         const filepaths = files.documents.map(doc => doc.filepath);
-        const filenames = files.documents.map(doc => doc.originalFilename);
-        console.log("here");
         const results = await Promise.all(filepaths.map(async (path) => {
-            console.log(path);
             return await textAnalyzer.processFile(path);
         }));
 
--- index.js
+++ index.js
@@ -2,17 +2,25 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
 
 app.use(cors());
 app.use(morgan('dev'));
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
 
 
 app.get('/', (req, res) => {
     res.send("<h1><center>Server is working properly</center></h1>");
 })
+
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
 
 const textRouter = require('./routers/textRouter');
 app.use('/api/v1', textRouter);
